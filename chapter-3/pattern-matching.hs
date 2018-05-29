c055e94d490e9ed1eb781ee4336d800c74f75045
Arialdo Martini <arialdomartini@gmail.com>
2018-05-29 22:45:28 +0200

    Playign with functions and pattern matching

--- chapter-3/pattern-matching.hs
+++ chapter-3/pattern-matching.hs
@@ -16,3 +16,18 @@ addVectors' (x1, y1) (x2, y2) = (x1 + x2, y1 + y2)
 -- pattern matching inside a list comprehension
 sumPairs :: [(Int, Int)] -> [Int]
 sumPairs pairs = [a + b | (a, b) <- pairs]
+
+bmiCalculator :: Double -> Double -> String
+bmiCalculator weight height
+  | bmi <= skinny = "Eat more!"
+  | bmi <= normal = "Ok"
+  | bmi <- max   = "Do some workout"
+  | otherwise   = "Uhm..."
+  where bmi = weight / height ^ 2
+        (skinny, normal, max) = (18.5, 25, 30)
+
+getInitials :: String -> String -> String
+getInitials firstname lastname = [f] ++ " " ++ [l]
+                                      where (f:_) = firstname
+                                            (l:_) = lastname
+  
