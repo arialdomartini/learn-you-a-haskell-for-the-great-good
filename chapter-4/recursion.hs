16455065b2a340132c6088c5ff4fe4046bda3b6c
Arialdo Martini <arialdomartini@gmail.com>
2018-06-09 17:10:44 +0200

    Take n elements from a list

--- chapter-4/recursion.hs
+++ chapter-4/recursion.hs
@@ -2,3 +2,11 @@ maximum' :: (Ord a) => [a] -> a
 maximum' [] = error "Cannot calculate maximum on an empty list"
 maximum' [x] = x
 maximum' (x:xs) = max x (maximum' xs)
+
+replicate' :: Int -> a -> [a]
+replicate' 1 element = [element]
+replicate' n element = element : (replicate' (n - 1) element)
+
+take' :: Int -> [a] -> [a]
+take' 0 _ = []
+take' n (x:xs) = x : ( take (n-1) xs )
