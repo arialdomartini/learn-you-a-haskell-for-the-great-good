0eb54df45991c5f142a34eb2a9230a97826686ea
Arialdo Martini <arialdomartini@gmail.com>
2018-06-09 17:20:06 +0200

    take, implemented with a case

--- chapter-4/recursion.hs
+++ chapter-4/recursion.hs
@@ -10,3 +10,9 @@ replicate' n element = element : (replicate' (n - 1) element)
 take' :: Int -> [a] -> [a]
 take' 0 _ = []
 take' n (x:xs) = x : ( take (n-1) xs )
+
+take2' :: Int -> [a] -> [a]
+take2' n xs
+  | length xs == 0  = []
+  | n == 0 = []
+  | otherwise = head(xs) : take2' (n-1) (tail xs)
