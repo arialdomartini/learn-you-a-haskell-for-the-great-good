9c84b5aecb9eae7853bdb349ddaf08e597f87948
Arialdo Martini <arialdomartini@gmail.com>
2018-06-10 08:26:41 +0200

    Partial application of infix functions with sections

--- chapter-5/curried.hs
+++ chapter-5/curried.hs
@@ -10,4 +10,12 @@ sub' x y = x - y
 s1 = flip' sub'
 s2 = flip'' sub'
 
+divideByTen :: (Floating a) => a -> a
+divideByTen = (/10)
+
+divideTenBy :: (Floating a) => a -> a
+divideTenBy = (10/)
+
+useDivideByTen = divideByTen 40 -- should be 4.0
+useDivideTenBy = divideTenBy 40 -- should be 2.5
 
