713e921566df6fbd3416e718f28f0a0552f9b84b
Arialdo Martini <arialdomartini@gmail.com>
2018-06-10 08:33:37 +0200

    Sections on non-infix partially applied functions

--- chapter-5/curried.hs
+++ chapter-5/curried.hs
@@ -1,3 +1,15 @@
+divideByTen :: (Floating a) => a -> a
+divideByTen = (/10)
+
+divideTenBy :: (Floating a) => a -> a
+divideTenBy = (10/)
+
+useDivideByTen = divideByTen 40 -- should be 4.0
+useDivideTenBy = divideTenBy 40 -- should be 2.5
+
+
+-- Applying sections on non-infix partially applied functions
+
 flip' :: (a -> b -> c) -> (b -> a -> c)
 flip' f x y = f y x
 
@@ -10,12 +22,11 @@ sub' x y = x - y
 s1 = flip' sub'
 s2 = flip'' sub'
 
-divideByTen :: (Floating a) => a -> a
-divideByTen = (/10)
 
-divideTenBy :: (Floating a) => a -> a
-divideTenBy = (10/)
+myDivide :: (Floating a) => a -> a -> a
+myDivide a b = a / b
 
-useDivideByTen = divideByTen 40 -- should be 4.0
-useDivideTenBy = divideTenBy 40 -- should be 2.5
+myDivideByTenFlipped = flip' myDivide
+myDivideByTen a = myDivideByTenFlipped 10
+useMyDivideByTen = myDivideByTen 40 -- should be 4.0
 
