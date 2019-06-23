4e8d17a806a5d016c6634d71482cd054a613ee22
Arialdo Martini <arialdomartini@gmail.com>
2019-06-23 22:09:01 +0200

    Reading chapter 1 again

--- chapter-1/lists.hs
+++ chapter-1/lists.hs
@@ -5,8 +5,10 @@ main =
 
 printList = print lostNumbers
 
-concatenate = lostNumbers ++ [9,10,11,12]
+concatenate = [1,2,3,4] ++ [9,10,11,12]
 cons = 0 : [1,2,3,4]
+
+stringsAreList = "Hello, world" == 'H' : ['e', 'l', 'l', 'o'] ++ [',', ' ', 'w', 'o', 'r', 'l', 'd']
 
 addLast = [1,2,3,4] ++ [5]
 
@@ -15,6 +17,16 @@ addLast = [1,2,3,4] ++ [5]
 
 accessingItem list position = list !! position
 access = accessingItem [1,2,15,18,0] 2 -- should be 15
+accessingChar index = "Hello, world!" !! index
+
+tryHead = head [1,2,3,4,5,6] == 1
+tryTail = tail [1,2,3,4,5,6] == [2,3,4,5,6]
+tryHeadTail list = list == head list : tail list
+tryReverse list = head (reverse list) == last list
+
+identity n = sum (take n (repeat 1)) -- a very inefficient function that returns its own Num argument
+
+
 
 accessRanges = tail (take 4 ['a'..'z']) -- should be ['b','c','d']
 
@@ -25,6 +37,8 @@ listComprehension3 = [x | x <- [50..100], x `mod` 7 == 3]
 listComprehension4 = [ if x < 10 then "bang" else "boom" | x <- [1..100], odd x]
 boomBangs xs =       [ if x < 10 then "bang" else "boom" | x <- xs, odd x]
 listComprehension5 = boomBangs [1..100]
+
+fizzBuzz n = take n [if x `mod` 15 == 0 then "fizzbuzz" else if x `mod` 3 == 0 then "fizz" else if x `mod` 5 == 0 then "buzz" else show x | x <- [1..100]]
 
 multiplePredicates = [x + y | x <- [1,2,3], y <-[1..100]]
 
