347cc48dfa1260294d9b2f4356f2bd8c8a1c2871
Arialdo Martini <arialdomartini@gmail.com>
2019-06-27 05:22:15 +0200

    ZipWith as a high order function

--- chapter-5/apply-twice.hs
+++ chapter-5/apply-twice.hs
@@ -2,7 +2,6 @@ applyNTimes :: (Eq n, Num n) => n -> (a -> a) -> (a -> a)
 applyNTimes 1 f x = f x
 applyNTimes n f x = applyNTimes (n-1) f (f x)
 
-
 applyTwice :: (a -> a) -> (a -> a)
 applyTwice f x = f ( f x)
 
@@ -13,3 +12,9 @@ example = twiceDouble 3 -- should be 12
 
 applyTwice' f = f . f
 example' = applyTwice' (\x -> 2 * x) 3
+
+
+zipWith' :: [a] -> [b] -> (a -> b -> c) -> [c]
+zipWith' [] _ _ = []
+zipWith' _ [] _ = []
+zipWith' (a:as) (b:bs) f = (f a b) : zipWith' as bs f
