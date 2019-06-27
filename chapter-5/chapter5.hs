4c1f2cfd70ac8724979926c34623810067179077
Arialdo Martini <arialdomartini@gmail.com>
2019-06-27 11:35:03 +0200

    calculating largest multiple

--- chapter-5/chapter5.hs
+++ chapter-5/chapter5.hs
@@ -11,4 +11,18 @@ quicksortFilter (x:xs) = quicksortFilter smaller ++ [x] ++ quicksortFilter bigge
 
 -- a String is already a list
 quicksortString :: String -> String
-quicksortString = quicksortFilter 
+quicksortString = quicksortFilter
+
+
+multiplesOf :: Int -> Int -> [Int]
+multiplesOf d top = [n | n <- [top, top-1..0], n `mod` d == 0]
+
+-- no matter the size of top, the execution is fast, because Haskell is lazy
+-- and the [top, top-1..0] is not entirely visited
+largestMultiple d top = head (multiplesOf d top)
+
+largestMultiple' d top = top - top `mod` d
+
+-- in this case, laziness doesn't help...
+largestMultipleReverse d top = head (multipleReversed d top)
+  where multipleReversed d top = reverse [n | n <- [0..top], n `mod` d == 0]
