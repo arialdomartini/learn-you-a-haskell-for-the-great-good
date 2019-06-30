3aad4e53c9b222a4555be00d0d1bfa906076b246
Arialdo Martini <arialdomartini@gmail.com>
2019-06-30 15:59:49 +0200

    Transpose from Data.List and manual implementation with foldr

--- chapter-6/modules.hs
+++ chapter-6/modules.hs
@@ -29,6 +29,7 @@ myIntersperse separator list = case list of
   [a] -> [a]
   (x:xs) -> x : separator : (myIntersperse separator xs)
 
+-- intercalate
 tryIntercalate = intercalate " " ["hello", "cruel", "world"] == "hello cruel world"
 
 myIntercalate :: [a] -> [[a]] -> [a]
@@ -36,3 +37,45 @@ myIntercalate separator lists = case lists of
   [] -> []
   [a] -> a
   (x:xs) -> x ++ separator ++ myIntercalate separator xs
+
+-- transpose
+tryTranspose = transpose
+  [ [1,2,3],
+    [10,20,30],
+    [100,200,300] ]
+  ==
+  [ [1,10,100],
+    [2,20,200],
+    [3,30,300] ]
+
+
+--myTranspose :: [[a]] -> [[a]]
+--myTranspose lists = myTranspose' lists []
+
+--myTranspose' [] = []
+--myTranspose' (x:xs) res = myTranspose' xs
+-- [1,2,3] -> [[1], [2], [3]]
+--transp :: [a] -> [[a]]
+transp [] = []
+transp (x:xs) = [x] : transp xs
+
+-- accum [[1], [2], [3]] -> [[10], [20], [30]] ->  [[1,10], [2,20], [3,30]]
+-- accum :: [[a]] -> [[a]] -> [[a]]
+-- accum [] _ = []
+-- accum _ [] = []
+-- accum (x:xs) (y:ys) =
+
+-- [ [1,2,3], [10,20,30], [100,200,300] ]
+-- [ [1,10,100], [2,20,200], [3,30,300] ]
+
+toListOfLists x = map (\e -> [e]) x
+
+-- done! Not that simple...
+
+transpose' xs = foldr (\e a -> aggregate e a) [] xs
+  where aggregate [] _ = []
+        aggregate (x:xs) [] = [x] : (aggregate xs [])
+        aggregate (x:xs) (y:ys) = (x:y) : aggregate xs ys
+
+tryTranspose' = transpose' [ [1,2,3],    [10,20,30], [100,200,300] ] ==
+                           [ [1,10,100], [2,20,200], [3,30,300] ] 
