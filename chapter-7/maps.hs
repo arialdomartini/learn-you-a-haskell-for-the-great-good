12b470e8b06c08be4fec352bf57e8e1fd2c3bb90
Arialdo Martini <arialdomartini@gmail.com>
2019-07-07 22:56:53 +0200

    findKey with filter and function composition

--- chapter-7/maps.hs
+++ chapter-7/maps.hs
@@ -11,3 +11,7 @@ findKey k xs = case xs of
     if k == fst x
     then Just (snd x)
     else findKey k xs
+
+findKey' :: (Eq k) => k -> [(k, v)] -> v
+--findKey' k = snd . head . filter (\x -> fst x == k)
+findKey' k = snd . head . filter (\x -> fst x == k)
