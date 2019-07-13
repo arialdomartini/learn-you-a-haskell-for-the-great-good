b7a02ef77a3a9d3c294c5ff623b4be360c0721bf
Arialdo Martini <arialdomartini@gmail.com>
2019-07-13 03:51:49 +0200

    Pattern matching on a custom data constructor

--- test/Chapter8/OwnTypesSpec.hs
+++ test/Chapter8/OwnTypesSpec.hs
@@ -33,5 +33,11 @@ data Shape = Triangle Base Height | Rectangle Point Point deriving Show
 
 surface :: Shape -> Float
 surface (Triangle b h) = b * h / 2
-surface (Rectangle (Point x1 y1) (Point x2 y2)) = (abs $ x2 - x1) * (abs $ y2 - y1)
+surface (Rectangle p1 p2) = base * height
+  where base = distX p1 p2
+        height = distY p1 p2
 
+
+distX (Point x1 _) (Point x2 _) = x2 -! x1
+distY (Point _ y1) (Point _ y2) = y2 -! y1
+(-!) a b = abs $ b - a
