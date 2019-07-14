c861e83d3a1b1c4687b6775c4778047e582b62f2
Arialdo Martini <arialdomartini@gmail.com>
2019-07-14 17:18:56 +0200

    elem' detects if an element belongs to a binary tree

--- test/Chapter8/BinaryTreeSpec.hs
+++ test/Chapter8/BinaryTreeSpec.hs
@@ -9,8 +9,14 @@ spec = do
     singleton 2 `shouldBe` (Node 2 Empty Empty)
 
   it "should insert values in a tree" $ do
-    insert  3( insert 5 (insert 20 (singleton 10))) `shouldBe`
+    insert 3 ( insert 5 (insert 20 (singleton 10))) `shouldBe`
       (Node 10 (Node 5 (Node 3 Empty Empty) Empty) (Node 20 Empty Empty))
+
+  it "should detect if an element is in a tree" $ do
+    elem' 2 (insert 1 (insert 2 (singleton 3))) `shouldBe` True
+    elem' 10 (insert 1 (insert 2 (singleton 3))) `shouldBe` False
+
+
 
 singleton :: a -> Tree a
 singleton x = Node x Empty Empty
@@ -20,3 +26,7 @@ insert a Empty = singleton a
 insert a (Node x left right) = if a > x
   then Node x left (insert a right)
   else Node x (insert a left) right
+
+elem' :: (Eq a) => a -> Tree a -> Bool
+elem' e Empty = False
+elem' e (Node x left right) = if e == x then True else elem' e left || elem' e right
