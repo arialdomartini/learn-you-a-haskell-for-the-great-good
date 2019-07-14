dca010ea2b1a8e63933cd6fa1438ba2e137b02b2
Arialdo Martini <arialdomartini@gmail.com>
2019-07-14 15:52:46 +0200

    Builds a list with :>

--- test/Chapter8/RecursiveSpec.hs
+++ test/Chapter8/RecursiveSpec.hs
@@ -4,11 +4,20 @@ import Test.Hspec
 
 main = hspec spec
 spec = do
-  it "should pass" $ do
-    toList (Cons 1 (Cons 2 (Cons 3 (Empty)))) `shouldBe` [1,2,3]
+--  it "should build a list" $ do
+--    toList (Cons 1 (Cons 2 (Cons 3 (Empty)))) `shouldBe` [1,2,3]
+
+  it "should build a list with :>" $ do
+    toList (1 :> 2 :> 3 :> Empty) `shouldBe` [1,2,3]
 
 
-data List a = Empty | Cons a (List a)
+infixr 5 :>
+data List a = Empty | a :> (List a)
 toList :: List a -> [a]
 toList Empty = []
-toList (Cons a rest) = a : (toList rest)
+toList (a :> rest) = a : (toList rest)
+
+-- alternatively
+-- data List a = Empty | Cons a (List a)
+-- (:>) :: a -> List a -> List a
+-- (:>) a l = Cons a l
