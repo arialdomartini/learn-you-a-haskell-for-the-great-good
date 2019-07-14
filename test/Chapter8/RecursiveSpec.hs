f3d4fe8ad41f8e767076e1bf98955c72ba75cb6c
Arialdo Martini <arialdomartini@gmail.com>
2019-07-14 16:06:54 +0200

    Concatenation of Lists

--- test/Chapter8/RecursiveSpec.hs
+++ test/Chapter8/RecursiveSpec.hs
@@ -10,9 +10,11 @@ spec = do
   it "should build a list with :>" $ do
     toList (1 :> 2 :> 3 :> Empty) `shouldBe` [1,2,3]
 
+  it "should concatenate lists" $ do
+    ( (1:>2:>Empty) .++ (3:>4:>Empty) ) `shouldBe` ( 1:>2:>3:>4:>Empty )
 
 infixr 5 :>
-data List a = Empty | a :> (List a)
+data List a = Empty | a :> (List a) deriving (Eq, Ord, Show)
 toList :: List a -> [a]
 toList Empty = []
 toList (a :> rest) = a : (toList rest)
@@ -21,3 +23,8 @@ toList (a :> rest) = a : (toList rest)
 -- data List a = Empty | Cons a (List a)
 -- (:>) :: a -> List a -> List a
 -- (:>) a l = Cons a l
+-- but this wouldn't have worked with pattern matching!
+
+(.++) :: List a -> List a -> List a
+Empty .++ bs = bs
+(a:>as) .++ bs = a:>(as .++ bs)
