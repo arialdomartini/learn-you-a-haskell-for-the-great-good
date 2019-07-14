a0eb22ef2c0f170d9ade0ac84a524d76069c0dc6
Arialdo Martini <arialdomartini@gmail.com>
2019-07-14 17:56:06 +0200

    Typeclass subclass of another typeclass (using a type constraint)

--- test/Chapter8/EquatableSpec.hs
+++ test/Chapter8/EquatableSpec.hs
@@ -11,7 +11,11 @@ spec = do
     show Red `shouldBe` "Red light, stop"
     show Green `shouldBe` "Green light, go!"
     show Yellow `shouldBe` "Yellow light, wait..."
-    
+
+  it "can compare 2 Maybes" $ do
+    Just 2 ==. Just 2 `shouldBe` True
+
+
 data Semaphore = Red | Green | Yellow
 
 class Eq' a where
@@ -30,3 +34,8 @@ instance Show Semaphore where
   show Red = "Red light, stop"
   show Green = "Green light, go!"
   show Yellow = "Yellow light, wait..."
+
+instance (Eq a) => Eq' (Maybe a) where
+  Nothing ==. Nothing = True
+  Just a ==. Just b = a == b
+  _ ==. _ = False
