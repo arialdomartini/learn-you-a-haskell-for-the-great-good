8203c783811d0a940f747cb36f9ee6bfbe64a8f6
Arialdo Martini <arialdomartini@gmail.com>
2019-07-14 23:07:51 +0200

    Maybe is a functor

--- test/Chapter8/FunctorSpec.hs
+++ test/Chapter8/FunctorSpec.hs
@@ -6,8 +6,16 @@ spec = do
   it "List should be a functor" $ do
     fmap' (*2) [1,2,3] `shouldBe` [2,4,6]
 
+  it "Maybe should be a functor" $ do
+    fmap' (+10) (Just 8) `shouldBe` Just 18
+    fmap' (+10) Nothing `shouldBe` Nothing
+
 class Functor' f where
   fmap' :: (a -> b) -> f a -> f b
 
 instance Functor' [] where
   fmap' g f = map g f
+
+instance Functor' Maybe where
+  fmap' _ Nothing = Nothing
+  fmap' g (Just x) = Just (g x)
