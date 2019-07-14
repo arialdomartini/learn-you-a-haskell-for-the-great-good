3faf4746519a0573b64a49a85c8d7c7b32853cff
Arialdo Martini <arialdomartini@gmail.com>
2019-07-14 17:43:45 +0200

    Semaphone implements Show

--- test/Chapter8/EquatableSpec.hs
+++ test/Chapter8/EquatableSpec.hs
@@ -7,6 +7,11 @@ spec = do
     Red ==. Green `shouldBe` False
     Red ==. Red `shouldBe` True
 
+  it "should show semaphore states" $ do
+    show Red `shouldBe` "Red light, stop"
+    show Green `shouldBe` "Green light, go!"
+    show Yellow `shouldBe` "Yellow light, wait..."
+    
 data Semaphore = Red | Green | Yellow
 
 class Eq' a where
@@ -20,3 +25,8 @@ instance Eq' Semaphore where
   Green ==. Green = True
   Yellow ==. Yellow = True
   _ ==. _ = False
+
+instance Show Semaphore where
+  show Red = "Red light, stop"
+  show Green = "Green light, go!"
+  show Yellow = "Yellow light, wait..."
