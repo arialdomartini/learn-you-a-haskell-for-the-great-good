de15d526418df795bc9a50d7e484b08bd0324da0
Arialdo Martini <arialdomartini@gmail.com>
2019-07-15 22:26:47 +0200

    Map is an instance of Functor'

--- test/Chapter8/FunctorSpec.hs
+++ test/Chapter8/FunctorSpec.hs
@@ -1,6 +1,8 @@
 module Chapter8.FunctorSpec where
 import Test.Hspec
 import qualified Chapter8.BinaryTreeSpec as Tree
+import qualified Data.Map as Map
+
   
 spec = do
   it "List should be a functor" $ do
@@ -21,6 +23,10 @@ spec = do
         expectedTree = Tree.insert 6 ( Tree.insert 10 (Tree.insert 40 (Tree.singleton 20)))
       in fmap' (*2) tree `shouldBe` expectedTree
 
+
+  it "Map should be a functor" $ do
+    fmap' (*2) (Map.fromList [("one", 1), ("two", 2)]) `shouldBe` (Map.fromList [("one", 2), ("two", 4)])
+
 class Functor' f where
   fmap' :: (a -> b) -> f a -> f b
 
@@ -39,3 +45,8 @@ data Either' a b = Left' a | Right' b deriving (Eq, Show)
 instance Functor' (Either' a) where
   fmap' f (Left' x) = Left' x
   fmap' f (Right' x) = Right' (f x)
+
+instance (Ord k) => Functor' (Map.Map k) where
+  fmap' f m = Map.fromList (recur f (Map.toList m)) where
+    recur f [] = []
+    recur f (h:tail) = ((fst h, f (snd h)) : (recur f tail))
