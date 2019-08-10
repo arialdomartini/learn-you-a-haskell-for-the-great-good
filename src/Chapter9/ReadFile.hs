1ae1f1add97786d9e455042959e47a086a274693
Arialdo Martini <arialdomartini@gmail.com>
2019-08-10 10:44:24 +0200

    Reads a file using withFile

--- src/Chapter9/ReadFile.hs
+++ src/Chapter9/ReadFile.hs
@@ -3,7 +3,14 @@ module Chapter9.ReadFile where
 import System.IO
 
 main = do
+  -- using openFile
   handle <- openFile "src/Chapter9/ReadFile.hs" ReadMode
   content <- hGetContents handle
   putStrLn content
   hClose handle
+
+  -- using withFile
+  withFile "src/Chapter9/ReadFile.hs"
+    ReadMode (\handle -> do
+                 content <- hGetContents handle
+                 putStrLn content)
