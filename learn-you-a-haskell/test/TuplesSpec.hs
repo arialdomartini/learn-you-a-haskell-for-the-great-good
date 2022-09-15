5270606a6b90663de6e5a3717beec3e7841a8c16
Arialdo Martini <arialdomartini@gmail.com>
2022-09-15 23:09:27 +0200

    Creates a tuple using zip

--- learn-you-a-haskell/test/TuplesSpec.hs
+++ learn-you-a-haskell/test/TuplesSpec.hs
@@ -7,3 +7,7 @@ spec = do
   it "composes and decomposes tuples" $ do
     let t = ("foo", 42) :: (String, Int)
     (fst t, snd t) `shouldBe` t
+
+  it "builds tuples using zip" $ do
+    let f = \a b -> head $ zip [a] [b]
+    (f "foo" 42) `shouldBe` (("foo", 42) :: (String, Int))
