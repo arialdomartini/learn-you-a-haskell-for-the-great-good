b73a024731b8331eb837d0dcb52a334ee8d22719
Arialdo Martini <arialdomartini@gmail.com>
2022-09-17 00:32:23 +0200

    A tuple of bounded types is also bounded

--- learn-you-a-haskell/test/TuplesSpec.hs
+++ learn-you-a-haskell/test/TuplesSpec.hs
@@ -11,3 +11,9 @@ spec = do
   it "builds tuples using zip" $ do
     let f = \a b -> head $ zip [a] [b]
     (f "foo" 42) `shouldBe` (("foo", 42) :: (String, Int))
+
+
+  it "tuple of bounded values is bounded" $ do
+    let m = maxBound :: (Bool, Int, Char)
+        n = (maxBound, maxBound, maxBound) :: (Bool, Int, Char)
+    n `shouldBe` m
