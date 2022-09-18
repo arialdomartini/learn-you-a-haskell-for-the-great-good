20d5a3c8cea21bdeb651c919ea4f735b3d55d205
Arialdo Martini <arialdomartini@gmail.com>
2022-09-18 15:02:37 +0200

    Another recursive function for calculating maximum

--- learn-you-a-haskell/test/RecursionSpec.hs
+++ learn-you-a-haskell/test/RecursionSpec.hs
@@ -17,6 +17,13 @@ myMax xs =
     max' [] m = m
     max' (x:t) m = if x > m then max' t x else max' t m
 
+myMax' :: [Int] -> Int
+myMax' [] = error "Cannot calculate the maximum element of an empty list"
+myMax' [m] = m
+myMax' (x:t) =
+  greatestBetween x (myMax' t)
+  where greatestBetween a b = if a > b then a else b
+
 spec :: Spec
 spec = do
   it "calculates the fibonacci series" $ do
@@ -25,3 +32,7 @@ spec = do
   it "calculates the maximum of a list using recursion" $ do
     let xs = [1,6,2,66,66,12,54] :: [Int]
       in (myMax xs) `shouldBe` (maximum xs)
+
+  it "calculates the maximum of a list using a simplified recursion" $ do
+    let xs = [1,6,2,66,66,12,54] :: [Int]
+      in (myMax' xs) `shouldBe` (maximum xs)
