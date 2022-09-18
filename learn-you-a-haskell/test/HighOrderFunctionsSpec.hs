88e6f0b946c0236d7047eea5ff4fe1e4dece5bb7
Arialdo Martini <arialdomartini@gmail.com>
2022-09-18 19:19:03 +0200

    Sections for non-infix functions

--- learn-you-a-haskell/test/HighOrderFunctionsSpec.hs
+++ learn-you-a-haskell/test/HighOrderFunctionsSpec.hs
@@ -8,10 +8,17 @@ mult3 x y z = x * y * z
 divideBy10 :: Fractional a => a -> a
 divideBy10 = (/10)
 
+isAlpha :: Char -> Bool
+isAlpha = (`elem` ['a'..'z'])
+
 spec :: Spec
 spec = do
   it "implicitly curries functions" $ do
     mult3 (2::Int) 3 4 `shouldBe` (((mult3 2) 3) 4)
 
-  it "supports sections" $ do
+  it "supports sections for infix functions" $ do
     (divideBy10 (120::Double)) `shouldBe` (120 / 10)
+
+  it "support sections for prefix functions" $ do
+    (isAlpha 'a') `shouldBe` True
+    (isAlpha '.') `shouldBe` False
