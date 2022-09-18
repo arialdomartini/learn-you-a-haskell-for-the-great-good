f696e5678ee7494631cf84b58ef7ff1122f88193
Arialdo Martini <arialdomartini@gmail.com>
2022-09-18 19:36:48 +0200

    Implements zipWith as a high-order (recursive) function

--- learn-you-a-haskell/test/HighOrderFunctionsSpec.hs
+++ learn-you-a-haskell/test/HighOrderFunctionsSpec.hs
@@ -14,6 +14,11 @@ isAlpha = (`elem` ['a'..'z'])
 twice :: (a -> a) -> (a -> a)
 twice f a = f (f a)
 
+zipWith' :: (a -> b -> c) -> [a] -> [b] -> [c]
+zipWith' _ [] _ = []
+zipWith' _ _ [] = []
+zipWith' f (a:as) (b:bs) = (f a b) : zipWith' f as bs
+
 spec :: Spec
 spec = do
   it "implicitly curries functions" $ do
@@ -31,3 +36,8 @@ spec = do
         ff = twice f
     (ff "hey") `shouldBe` "hey!!"
     ((twice (*2)) 3) `shouldBe` (3 * 2 * 2 :: Int)
+
+  it "defines zipWith as high-order function" $ do
+    let as = [1,2,3] :: [Int]
+    let bs = [2,3,4]
+    (zipWith' (+) as bs) `shouldBe` (zipWith (+) as bs)
