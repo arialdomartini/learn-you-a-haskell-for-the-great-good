b6dda9068e9e3de6346f56ce335e420d58a328e8
Arialdo Martini <arialdomartini@gmail.com>
2022-09-18 18:27:35 +0200

    (/10) is a section

--- learn-you-a-haskell/test/HighOrderFunctionsSpec.hs
+++ learn-you-a-haskell/test/HighOrderFunctionsSpec.hs
@@ -5,7 +5,13 @@ import Test.Hspec
 mult3 :: Num a => a -> (a -> (a -> a))
 mult3 x y z = x * y * z
 
+divideBy10 :: Fractional a => a -> a
+divideBy10 = (/10)
+
 spec :: Spec
 spec = do
   it "implicitly curries functions" $ do
     mult3 (2::Int) 3 4 `shouldBe` (((mult3 2) 3) 4)
+
+  it "supports sections" $ do
+    (divideBy10 (120::Double)) `shouldBe` (120 / 10)
