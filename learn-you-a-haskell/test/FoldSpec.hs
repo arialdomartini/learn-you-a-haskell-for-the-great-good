809b8bc9ac95d1bd915492b9ad9d4406e31e93b5
Arialdo Martini <arialdomartini@gmail.com>
2022-09-19 23:33:42 +0200

    reverse implemented with foldl

--- learn-you-a-haskell/test/FoldSpec.hs
+++ learn-you-a-haskell/test/FoldSpec.hs
@@ -8,6 +8,9 @@ sum' = foldl1 (+)
 maximum' :: Ord a => [a] -> a
 maximum' = foldl1 (max) -- does not work with empty lists!
 
+reverse' :: [a] -> [a]
+reverse' = foldl (\a x -> x : a) []
+
 spec :: Spec
 spec = do
   it "sums elements in a list" $ do
@@ -16,3 +19,6 @@ spec = do
   it "maximum with foldl" $ do
     (maximum' [1,5,6,2,44,2]) `shouldBe` (44 :: Int)
     (maximum' [1]) `shouldBe` (1 :: Int)
+
+  it "reverse implemented with foldl" $ do
+    (reverse' ['a', 'b', 'c']) `shouldBe` ['c', 'b', 'a']
