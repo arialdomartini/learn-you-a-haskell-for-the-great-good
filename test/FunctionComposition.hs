70b19a572efb4a577f0750ba9459dd1fa170be79
Arialdo Martini <arialdomartini@gmail.com>
2022-09-25 17:19:45 +0200

    Free-Point style

--- test/FunctionComposition.hs
+++ test/FunctionComposition.hs
@@ -1,4 +1,6 @@
 {-# OPTIONS_GHC -Wno-type-defaults #-}
+{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
+{-# HLINT ignore "Redundant bracket" #-}
 module FunctionComposition where
 
 import Test.Hspec
@@ -38,3 +40,25 @@ spec = do
       composition :: [Int]
       composition = replicate 2 . product . map (*3) $ zipWith max [1,2] [4,5]
       in invocation `shouldBe` composition
+
+  it "can use point free style" $ do
+    let
+      sum' :: Num a => [a] -> a
+      sum' = foldl1 (+)
+      in sum' [1,2,3] `shouldBe` 1 + 2 + 3
+
+  it "another example of point free style" $ do
+    let
+      fn x = ceiling (negate (tan (cos (max 50 x))))
+      fnPointFree x = ceiling . negate . tan . cos $ max 50 x
+      in fn 100 `shouldBe` fnPointFree 100
+
+
+  it "3rd example of point-free style" $ do
+    let
+      oddSquareSum :: Integer
+      oddSquareSum = sum (takeWhile (<10000) (filter odd (map (^2) [1..])))
+
+      fnPointFree :: Integer
+      fnPointFree = sum . (takeWhile (<10000)). (filter  odd) $ map (^2) [1..]
+      in oddSquareSum `shouldBe` fnPointFree
