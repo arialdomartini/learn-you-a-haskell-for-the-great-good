b4225aa8304f590da693cc9e226836e5e3c0c16c
Arialdo Martini <arialdomartini@gmail.com>
2022-09-25 22:45:21 +0200

    words implemented manually

--- test/SystemModuleSpec.hs
+++ test/SystemModuleSpec.hs
@@ -16,3 +16,23 @@ spec = do
       nub' (x:xs) = x : nub'  (deduped x xs)
 
       in nub' cs `shouldBe` nub cs
+
+  it "counts words" $ do
+    let
+      words'' :: [String] -> [Char] -> [String]
+      words'' acc [] = acc
+      words'' [] (x:xs) =
+        if x == ' '
+        then words'' [] xs
+        else words'' [[x]] xs
+
+      words'' acc@(a:as) (x:xs) =
+        if x == ' '
+        then words'' ([] : acc) xs
+        else words'' ((a ++ [x]) : as) xs
+
+      words' :: [Char] -> [String]
+      words' = reverse . words'' [[]]
+
+      sentence = "nel mezzo del cammin di nostra vita"
+      in words' sentence `shouldBe` words sentence
