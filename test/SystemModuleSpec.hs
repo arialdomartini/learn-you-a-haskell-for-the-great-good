ed254e225d233b56f0d48144a9ca14d56ca59a04
Arialdo Martini <arialdomartini@gmail.com>
2022-09-25 23:48:11 +0200

    Counts word occurrences using function composition

--- test/SystemModuleSpec.hs
+++ test/SystemModuleSpec.hs
@@ -4,6 +4,30 @@ module SystemModuleSpec where
 import Test.Hspec
 import Data.List (nub)
 import GHC.OldList (group)
+import Data.List (sort)
+
+group' :: Eq a => [a] -> [[a]]
+group' [] = []
+group' xs@(x:_) =
+  let (e, ne) = span (==x) xs
+  in e : group' ne
+
+
+words'' :: [String] -> [Char] -> [String]
+words'' acc [] = acc
+words'' [] (x:xs) =
+  if x == ' '
+  then words'' [] xs
+  else words'' [[x]] xs
+
+words'' acc@(a:as) (x:xs) =
+  if x == ' '
+  then words'' ([] : acc) xs
+  else words'' ((a ++ [x]) : as) xs
+
+words' :: String -> [String]
+words' = reverse . words'' [[]]
+
 
 spec :: Spec
 spec = do
@@ -20,31 +44,16 @@ spec = do
 
   it "counts words" $ do
     let
-      words'' :: [String] -> [Char] -> [String]
-      words'' acc [] = acc
-      words'' [] (x:xs) =
-        if x == ' '
-        then words'' [] xs
-        else words'' [[x]] xs
-
-      words'' acc@(a:as) (x:xs) =
-        if x == ' '
-        then words'' ([] : acc) xs
-        else words'' ((a ++ [x]) : as) xs
-
-      words' :: [Char] -> [String]
-      words' = reverse . words'' [[]]
-
       sentence = "nel mezzo del cammin di nostra vita"
       in words' sentence `shouldBe` words sentence
 
   it "groups elements in a list" $ do
     let
-      group' :: Eq a => [a] -> [[a]]
-      group' [] = []
-      group' xs@(x:_) =
-        let (e, ne) = span (==x) xs
-        in e : group' ne
-
       ns = [1,1,1,1,2,2,2,2,3,3,2,2,2,5,6,7, 1, 1, 1] :: [Int]
       in group ns `shouldBe` group' ns
+
+  it "count word occurrences" $ do
+    let
+      countWords' :: String -> [(String, Int)]
+      countWords' = fmap (\g -> (head g, length g)) . group' . sort . words'
+      in countWords' "ciao mamma come stai mamma ciao ciao" `shouldBe` [("ciao", 3 :: Int), ("come", 1), ("mamma", 2), ("stai", 1)]
