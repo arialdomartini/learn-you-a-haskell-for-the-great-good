1ec61ec2768efbcecd828eda25049e7540139cd3
Arialdo Martini <arialdomartini@gmail.com>
2022-09-28 23:21:16 +0200

    Defining custom data types

---
+++ test/OwnTypesSpec.hs
@@ -0,0 +1,15 @@
+module OwnTypesSpec where
+
+import Test.Hspec
+
+data Shape = Circle Float Float Float | Rectangle Float Float
+
+area :: Shape -> Float
+area (Circle _ _ r) = pi * r ^ (2:: Int)
+area (Rectangle b h) = b * h
+
+spec :: Spec
+spec = do
+  it "uses a custom type" $ do
+     (area (Circle 1 2 3)) `shouldBe` 3 * 3 * pi
+     (area (Rectangle 8 9)) `shouldBe` 8 * 9
