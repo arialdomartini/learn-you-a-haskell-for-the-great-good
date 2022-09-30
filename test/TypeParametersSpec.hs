e5ab5820e77d7f4f3843b85bde81cd98816fb274
Arialdo Martini <arialdomartini@gmail.com>
2022-09-30 21:00:43 +0200

    Scalar product

--- test/TypeParametersSpec.hs
+++ test/TypeParametersSpec.hs
@@ -3,8 +3,12 @@ module TypeParametersSpec where
 import Test.Hspec
 
 data Vector a = Vector a a a deriving (Show, Eq)
+
 sum' :: (Num a) =>  Vector a -> Vector a -> Vector a
 sum' (Vector x1 y1 z1) (Vector x2 y2 z2) = Vector (x1+x2) (y1+y2) (z1+z2)
+
+dotProd' :: (Num a) => Vector a -> Vector a-> a
+dotProd' (Vector x1 y1 z1) (Vector x2 y2 z2) = (x1*x2) + (y1*y2) + (z1*z2)
 
 spec :: Spec
 spec = do
@@ -19,3 +23,8 @@ spec = do
     let v = Vector 1 1 1 :: Vector Int
         w = Vector 2 0 2 :: Vector Int
         in (v `sum'` w) `shouldBe` Vector 3 1 3
+
+  it "dotprod vectors" $ do
+    let v = Vector 1 1 1 :: Vector Int
+        w = Vector 2 0 2 :: Vector Int
+        in (v `dotProd'` w) `shouldBe` 1*2 + 1*0 + 1*2
