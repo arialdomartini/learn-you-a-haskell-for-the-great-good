f9ff70fbbba59fab08ece640c59f5055c350c90e
Arialdo Martini <arialdomartini@gmail.com>
2022-09-30 19:27:35 +0200

    Sum of vectors

--- test/RecordSpec.hs
+++ test/RecordSpec.hs
@@ -5,20 +5,14 @@ module RecordSpec where
 import Test.Hspec
 import Record2
 
-data Person =
-  Person
-  {
-    firstName :: String
-  , secondName :: String
-  , height :: Int }
-
+data Person = Person { firstName :: String }
 
 spec :: Spec
 spec = do
   it "can use records" $ do
     -- fields are not positional
     let
-      john = Person { secondName = "Doh", firstName = "John", height = 182 }
+      john = Person { firstName = "John" }
       other = AnotherRecord { firstName = "foo" } in
       do
         RecordSpec.firstName john `shouldBe` "John"
--- test/TypeParametersSpec.hs
+++ test/TypeParametersSpec.hs
@@ -2,7 +2,9 @@ module TypeParametersSpec where
 
 import Test.Hspec
 
-data Vector a = Vector a a a deriving (Show)
+data Vector a = Vector a a a deriving (Show, Eq)
+sum' :: (Num a) =>  Vector a -> Vector a -> Vector a
+sum' (Vector x1 y1 z1) (Vector x2 y2 z2) = Vector (x1+x2) (y1+y2) (z1+z2)
 
 spec :: Spec
 spec = do
@@ -12,3 +14,8 @@ spec = do
          in do x `shouldBe` 1
                y `shouldBe` 2
                z `shouldBe` 3
+
+  it "sums vectors" $ do
+    let v = Vector 1 1 1 :: Vector Int
+        w = Vector 2 0 2 :: Vector Int
+        in (v `sum'` w) `shouldBe` Vector 3 1 3
