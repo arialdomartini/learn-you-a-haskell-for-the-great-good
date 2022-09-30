8df42786c6cd1a833c3648ae42550a89c8acee6c
Arialdo Martini <arialdomartini@gmail.com>
2022-09-30 09:36:47 +0200

    Records with identically-named fields raises compilation errors

--- test/RecordSpec.hs
+++ test/RecordSpec.hs
@@ -9,6 +9,18 @@ data Person =
   , secondName :: String
   , height :: Int }
 
+
+-- This record could not be defined, because it would create
+-- clashing functions
+-- A possible alternative approach is described here:
+-- https://downloads.haskell.org/ghc/latest/docs/users_guide/exts/duplicate_record_fields.html#duplicate-record-fields
+-- data RecordWithConflictingFieldName =
+--   AnotherRecord
+--   {
+--     firstName :: String
+--   , secondName :: String
+--   , height :: Int }
+
 spec :: Spec
 spec = do
   it "can use records" $ do
