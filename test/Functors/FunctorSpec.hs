5e6eab4310a9e8b0d03ebe3381bc2ca1fceabb7d
Arialdo Martini <arialdomartini@gmail.com>
2022-10-04 00:26:37 +0200

    Tree can be made an instance of Functor

--- test/Functors/FunctorSpec.hs
+++ test/Functors/FunctorSpec.hs
@@ -1,6 +1,7 @@
 module Functors.FunctorSpec where
 
 import Test.Hspec
+import Types.TreeSpec (Tree(Node), Tree(Empty))
 
 class Functor' f where
   fmap' :: (a -> b) -> f a -> f b
@@ -15,6 +16,10 @@ instance Functor' Maybe' where
   fmap' _ Nothing' = Nothing'
   fmap' f (Just' a) = Just' (f a)
 
+instance Functor' Tree where
+  fmap' _ Empty = Empty
+  fmap' f (Node v l r) = Node (f v) (fmap' f l) (fmap' f r)
+
 spec :: Spec
 spec = do
   it "maps on a list" $ do
@@ -24,3 +29,8 @@ spec = do
 
   it "Maybe is an instance of Functor" $ do
     fmap' (++ "!") (Just' "hey") `shouldBe` Just' "hey!"
+
+  it "Tree can be made an instance of Functor" $ do
+    let tree = Node (100 :: Int) (Node 90 (Node 80 Empty Empty) Empty) Empty
+        expected = Node (200 :: Int) (Node 180 (Node 160 Empty Empty) Empty) Empty
+    fmap' (*2) tree `shouldBe` expected
