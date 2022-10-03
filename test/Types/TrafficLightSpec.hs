1d79b339e140c52aeaba472d4b7aa5cd263da4f4
Arialdo Martini <arialdomartini@gmail.com>
2022-10-03 12:51:55 +0200

    Maybe' is an instance of Eq

--- test/Types/TrafficLightSpec.hs
+++ test/Types/TrafficLightSpec.hs
@@ -19,6 +19,15 @@ instance Show TrafficLight where
     show Yellow  = "Yellow light"
     show Green = "Green light"
 
+
+data Maybe' a = Nothing' | Just' a
+
+instance (Eq m) => Eq (Maybe' m) where
+  (==) (Just' l) (Just' r) = l == r
+  (==) Nothing' Nothing' = True
+  (==) Nothing' (Just' _) = False
+  (==) (Just' _) Nothing' = False
+
 spec :: Spec
 spec = do
   it "implements Eq" $ do
@@ -38,3 +47,13 @@ spec = do
     show Red `shouldBe` "Red light"
     show Yellow `shouldBe` "Yellow light"
     show Green `shouldBe` "Green light"
+
+  it "implements Eq on Maybe'" $ do
+    let j = Just' "hey"
+        x = Just' "joe"
+        n = Nothing' :: (Maybe' String)
+        in do j /= x `shouldBe` True
+              j /= j `shouldBe` False
+              j == n `shouldBe` False
+              j == j `shouldBe` True
+              n == n `shouldBe` True
