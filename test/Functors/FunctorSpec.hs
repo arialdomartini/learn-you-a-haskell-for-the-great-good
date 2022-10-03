37d1782ef5da9878b4e233e20091ed797eeb1d2c
Arialdo Martini <arialdomartini@gmail.com>
2022-10-04 00:14:56 +0200

    Maybe can be made an instance of Functor

--- test/Functors/FunctorSpec.hs
+++ test/Functors/FunctorSpec.hs
@@ -9,9 +9,18 @@ instance Functor' [] where
   fmap' _ [] = []
   fmap' f (h:t) = f h : fmap' f t
 
+data Maybe' a = Nothing' | Just' a deriving (Show, Eq)
+
+instance Functor' Maybe' where
+  fmap' _ Nothing' = Nothing'
+  fmap' f (Just' a) = Just' (f a)
+
 spec :: Spec
 spec = do
   it "maps on a list" $ do
     let l = [1,2,3] :: [Int]
         r = fmap' (*2) l
         in r `shouldBe` [2,4,6]
+
+  it "Maybe is an instance of Functor" $ do
+    fmap' (++ "!") (Just' "hey") `shouldBe` Just' "hey!"
