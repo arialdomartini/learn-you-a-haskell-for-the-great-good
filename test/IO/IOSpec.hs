4d1da4f954baa3bf366e9280c564e1e182004255
Arialdo Martini <arialdomartini@gmail.com>
2022-10-05 09:57:39 +0200

    An attempt to mock IO

--- test/IO/IOSpec.hs
+++ test/IO/IOSpec.hs
@@ -6,6 +6,11 @@ import Test.Hspec
 f :: IO String
 f = do return "mario"
 
+printHelloProduction :: IO()
+printHelloProduction = printHello putStrLn
+
+printHello :: (String -> IO ()) -> IO ()
+printHello pr =  pr "Hello, side-effectful world!"
 
 spec :: Spec
 spec = do
@@ -13,3 +18,10 @@ spec = do
   it "cracks open an IO" $ do
     result <- f
     result `shouldBe` "mario"
+
+  it "tests putStrLn" $ do
+    let pr s = do
+          s `shouldBe` "Hello, side-effectful world!"
+          return () -- putStrLn is mocked
+    result <- printHello pr
+    result `shouldBe` ()
