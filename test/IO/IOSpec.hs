a0b9924ee4f7e30dd2212b9aea3ce1766871db35
Arialdo Martini <arialdomartini@gmail.com>
2022-10-05 22:57:11 +0200

    Using a data type for stubs

--- test/IO/IOSpec.hs
+++ test/IO/IOSpec.hs
@@ -1,6 +1,8 @@
+{-# LANGUAGE GADTs #-}
 module IO.IOSpec where
 
 import Test.Hspec
+import Data.Char (toUpper)
 
 -- A function returning IO
 f :: IO String
@@ -12,8 +14,37 @@ printHelloProduction = printHello putStrLn
 printHello :: (String -> IO ()) -> IO ()
 printHello pr =  pr "Hello, side-effectful world!"
 
+
+saluteProd :: IO ()
+saluteProd = salute (Input {put = putStrLn, get= getLine})
+
+
+data Input where
+  Input :: {put :: String -> IO (), get :: IO String} -> Input
+
+salute :: Input -> IO ()
+salute input = do
+  put input "What's your first name?"
+  firstName <- get input
+  put input "What's your last name?"
+  lastName <- get input
+  let bigFirstName = map toUpper firstName
+      bigLastName = map toUpper lastName
+  put input $ "hey " ++ bigFirstName ++ " "
+    ++ bigLastName
+    ++ ", how are you?"
+
 spec :: Spec
 spec = do
+
+  it "stubs IO using a Data Type" $ do
+    let putStub s = return ()
+    let getStub = return "Mario"
+    let saluteTest = salute (Input {put = putStub, get= getStub})
+    result <- saluteTest
+    result `shouldBe` ()
+
+
   -- asserting an IO
   it "cracks open an IO" $ do
     result <- f
