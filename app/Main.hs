4e239d03ba8242a8cb80399d086cf4e0e2ad6a3a
Arialdo Martini <arialdomartini@gmail.com>
2022-10-06 00:03:25 +0200

    code from main to separate (IO) function

--- app/Main.hs
+++ app/Main.hs
@@ -2,10 +2,16 @@ module Main(main) where
 
 main :: IO ()
 main = do
-   putStrLn "Hello World"
-   putStrLn "Hello World. What's your name?"
-   name <- getLine
-   putStrLn $ "Hola, " ++ name
-   putStrLn $ tellFortune name
+   salute
+
+
+salute :: IO ()
+salute =
+  do putStrLn "Hello World"
+     putStrLn "Hello World. What's your name?"
+     name <- getLine
+     putStrLn $ "Hola, " ++ name
+     putStrLn $ tellFortune name
+
 
 tellFortune name = name ++ " you will be very lucky"
