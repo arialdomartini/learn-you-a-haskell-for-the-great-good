2d4b4cd53f3800455b4fa6b9125513dc2a423c2c
Arialdo Martini <arialdomartini@gmail.com>
2022-10-05 07:15:28 +0200

    Reads an input from stdin

--- app/Main.hs
+++ app/Main.hs
@@ -2,4 +2,7 @@ module Main(main) where
 
 main :: IO ()
 main = do
-  putStrLn "Hello World"
+   putStrLn "Hello World"
+   putStrLn "Hello World. What's your name?"
+   name <- getLine
+   putStrLn $ "Hola, " ++ name
