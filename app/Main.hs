a7f38bce6ff051c15293ae7b88acd451daea0ba2
Arialdo Martini <arialdomartini@gmail.com>
2022-10-05 08:36:54 +0200

    Separating pure from impure code

--- app/Main.hs
+++ app/Main.hs
@@ -6,3 +6,6 @@ main = do
    putStrLn "Hello World. What's your name?"
    name <- getLine
    putStrLn $ "Hola, " ++ name
+   putStrLn $ tellFortune name
+
+tellFortune name = name ++ " you will be very lucky"
