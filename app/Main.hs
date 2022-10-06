3fa8544c4ba9bd2f69ea8be4f7a624b4cd661b60
Arialdo Martini <arialdomartini@gmail.com>
2022-10-06 13:15:06 +0200

    An interactive menu in main

--- app/Main.hs
+++ app/Main.hs
@@ -1,10 +1,33 @@
 module Main(main) where
 
-main :: IO ()
-main = do
-  sequence forEverPrint
-  salute
+import qualified Data.Map as Map
+import Control.Monad(forever)
 
+choices = Map.fromList [
+  (1, ("forEverPrint", sequenceForEverPrint)),
+  (2, ("salute", salute))] :: Map.Map Int (String, IO ())
+
+main :: IO ()
+main = forever menu
+
+
+menu :: IO ()
+menu = do
+  putStrLn "1 forEverPrint"
+  putStrLn "2 salute"
+  choice <- getLine
+  let choiceN = (read choice) :: Int
+  let f = Map.lookup choiceN choices
+  case f of
+    Nothing -> putStrLn "Error: unknown choice."
+    Just f -> do
+      snd f
+      putStrLn "================"
+
+
+sequenceForEverPrint :: IO ()
+sequenceForEverPrint = do
+  sequence_ forEverPrint
 
 
 forEverPrint :: [IO ()]
