ed11a7562156875224385adb3fe4a2e9f2e0c80f
Arialdo Martini <arialdomartini@gmail.com>
2022-10-07 18:58:19 +0200

    Parses its command line args

--- app/Main.hs
+++ app/Main.hs
@@ -9,18 +9,40 @@ import GHC.IO.Handle
 import GHC.Show (intToDigit)
 import System.Environment (getArgs)
 
-choices :: Map.Map Int (String, IO ())
+choices :: Map.Map String (IO ())
 choices = Map.fromList [
-  (1, ("forEverPrint", sequenceForEverPrint)),
-  (2, ("salute", salute)),
-  (3, ("read input", readInput)),  -- run it with (echo 3 && cat README.md) | make run
-  (4, ("read input with getContents", readInputWithGetContents)),
-  (5, ("only short lines", shortLines)),
-  (6, ("read a file", readAFile)),
-  (7, ("copy a file", copyFile)),
-  (8, ("delete line from file", deleteLine)),
-  (9, ("print args", printArgs))
-  ]
+  ("for-ever-print", sequenceForEverPrint),
+  ("salute", salute),
+  ("read-input", readInput),  -- run it with (echo 3 && cat README.md) | make run
+  ("read-input-with-getContents", readInputWithGetContents),
+  ("only-short-lines", shortLines),
+  ("read-a-file", readAFile),
+  ("copy-a-file", copyFile),
+  ("delete-line-from-file", deleteLine),
+  ("print-args", printArgs)]
+
+
+main :: IO ()
+main = do
+  args <- getArgs
+  if args == []
+    then printHelp
+    else do let (command : t) = args
+            dispatch command t
+
+dispatch :: String -> [String] -> IO ()
+dispatch k args = do
+  let choice = Map.lookup k choices
+  case choice of
+    Nothing -> putStrLn "No command found"
+    Just command -> command
+
+printHelp :: IO ()
+printHelp = do sequence_ $ Map.mapWithKey printCommand choices
+
+printCommand :: String ->  IO () -> IO ()
+printCommand s c = putStrLn s
+
 
 printArgs :: IO ()
 printArgs = do
@@ -68,29 +90,11 @@ shortLines = interact $ onlyTakeShortLines
 onlyTakeShortLines :: String -> String
 onlyTakeShortLines = unlines . fmap (\s -> if length s > 10 then "too long" else s) . lines
 
-main :: IO ()
-main = forever menu
-
 readInput :: IO()
 readInput =
   forever $ do
     line <- getLine
     putStrLn $ map toUpper line
-
-menu :: IO ()
-menu = do
-  _ <- sequence $ Map.mapWithKey execCommand choices
-  choice <- getLine
-  let choiceN = (read choice) :: Int
-  let f = Map.lookup choiceN choices
-  case f of
-    Nothing -> putStrLn "Error: unknown choice."
-    Just f -> do
-      snd f
-      putStrLn "================"
-
-execCommand :: Int -> (String, IO ()) -> IO ()
-execCommand k (s, c) = putStrLn $ show k ++ ")" ++ " " ++ s
 
 
 sequenceForEverPrint :: IO ()
