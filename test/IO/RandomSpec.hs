37e05e7f09b16134736bd47ba62ebe51e7d378dc
Arialdo Martini <arialdomartini@gmail.com>
2022-10-07 22:48:58 +0200

    Passing stdGen to next random

--- test/IO/RandomSpec.hs
+++ test/IO/RandomSpec.hs
@@ -7,5 +7,7 @@ import System.Random.Stateful (mkStdGen)
 spec :: Spec
 spec = do
   it "generates pseudo random numbers" $ do
-    let (r, _) = random (mkStdGen 100) :: (Int, StdGen)
-    r `shouldBe` 9216477508314497915
+    let (r1, g1) = random (mkStdGen 100) :: (Int, StdGen)
+        (r2, _) = (random g1) :: (Int, StdGen)
+      in do r1 `shouldBe` 9216477508314497915
+            r2 `shouldBe` (-6917749724426303066)
