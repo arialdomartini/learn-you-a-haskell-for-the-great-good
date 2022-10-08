efa707d32b49834b4bd57c3b51d5ef844395a3c6
Arialdo Martini <arialdomartini@gmail.com>
2022-10-08 17:19:50 +0200

    Generates sequence of random numbers

--- test/IO/RandomSpec.hs
+++ test/IO/RandomSpec.hs
@@ -45,11 +45,18 @@ run (MyRand f) gen =
   let (v, _) = f gen
   in v
 
+randoms' :: StdGen -> [Int]
+randoms' gen =
+  r : rest where
+    (r, gen') = random gen :: (Int, StdGen)
+    rest = randoms' gen'
+
+
 spec :: Spec
 spec = do
   it "generates pseudo random numbers" $ do
     let (r1, g1) = random (mkStdGen 100) :: (Int, StdGen)
-        (r2, _) = (random g1) :: (Int, StdGen)
+        (r2, _) = random g1 :: (Int, StdGen)
       in do r1 `shouldBe` 9216477508314497915
             r2 `shouldBe` (-6917749724426303066)
 
@@ -66,3 +73,6 @@ spec = do
                                              in ((v', v''), s''))) :: (MyRand (Int, Int))
     let v = run r'' gen in
           v `shouldBe` (9216477508314497915,-6917749724426303066)
+
+  it "generates an infinite list of random number with a recursive function" $ do
+    take 4 (randoms' (mkStdGen 100)) `shouldBe` [9216477508314497915, -6917749724426303066, -2348976503111297336, -716157807093485800]
