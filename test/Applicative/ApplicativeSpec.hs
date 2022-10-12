d7ca068274a0a0ac64e0299d5344fedc95a6a315
Arialdo Martini <arialdomartini@gmail.com>
2022-10-12 23:26:22 +0200

    Making a function non-deterministic

--- test/Applicative/ApplicativeSpec.hs
+++ test/Applicative/ApplicativeSpec.hs
@@ -35,3 +35,10 @@ spec = do
 
   it "uses pure" $ do
      pure ("hey " ++) <*> pure "Joe"  `shouldBe` (pure "hey Joe" :: Maybe String)
+
+
+  it "lifts a function" $ do
+    let f a b c = (a,b,c) in
+      pure f <*> [1,2,3] <*> ["a", "b", "c"] <*> ['x', 'y'] `shouldBe` [(1,"a",'x'), (1,"a",'y'), (1,"b",'x'), (1,"b",'y') ,(1,"c",'x'), (1,"c",'y')
+                                                                       ,(2,"a",'x'), (2,"a",'y'), (2,"b",'x'), (2,"b",'y'), (2,"c",'x'), (2,"c",'y')
+                                                                       ,(3,"a",'x'), (3,"a",'y'), (3,"b",'x'), (3,"b",'y'), (3,"c",'x'), (3,"c",'y')]
