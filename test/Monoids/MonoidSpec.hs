8267f826dc77fcc03093bfe862b0cb57f49cf38e
Arialdo Martini <arialdomartini@gmail.com>
2022-10-15 18:37:09 +0200

    newtype for inverting type parameters to make type compatible with Functor

--- test/Monoids/MonoidSpec.hs
+++ test/Monoids/MonoidSpec.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE GADTs #-}
+{-# OPTIONS_GHC -Wno-name-shadowing #-}
 module Monoids.MonoidSpec where
 
 import Test.Hspec
@@ -15,9 +17,24 @@ f' :: A' -> B'
 -- f' s = B' $ getValueA s ++ "!"
 f' (A' s) = B' $ s ++ "!"
 
+newtype Pair b a = Pair (a, b) deriving (Show, Eq)
+instance Functor (Pair b)  where
+--fmap :: (a -> b) -> f a -> f b
+  fmap f (Pair (a,b)) = Pair (f a, b)
+
 spec :: Spec
 spec = do
   it "type alias are loosely checked" $ do
+     let s = "Hey" :: B
+         b = "Hey!" :: A
+      in f s `shouldBe` b  -- It works. Too bad.
+
+  it "newtypes are more strict" $ do
      let s = A' "Hey"
          b = B' "Hey!"
       in f' s `shouldBe` b
+
+  it "implements Functor for tuples using a newtype for inverting the type parameters" $ do
+    let pair = Pair ("Joe", "Hey")
+        f s = length s in
+      fmap f pair `shouldBe` Pair (3, "Hey")
