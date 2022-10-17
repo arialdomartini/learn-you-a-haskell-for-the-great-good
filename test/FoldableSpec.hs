eaa93e6a702cefd80118b32182ac746f8b042b4d
Arialdo Martini <arialdomartini@gmail.com>
2022-10-17 20:28:13 +0200

    Maybe as an instance of Foldable

--- test/FoldableSpec.hs
+++ test/FoldableSpec.hs
@@ -10,12 +10,20 @@ instance Foldable' [] where
   foldr' _ a [] = a
   foldr' f a (h:t) = foldr' f (f h a) t
 
+instance Foldable' Maybe where
+  foldr' _ a Nothing = a
+  foldr' f a (Just v) = f v a
 
 gauss :: Fractional a => a -> a
 gauss n = n * (n + 1) /2
 
 spec :: Spec
 spec = do
-  it "List can be made an instance of Foldable" $ do
+  it "List is an instance of Foldable" $ do
     foldr' (+) 0 [1..100] `shouldBe` gauss 100
     foldr' (*) 0 [1..50] `shouldBe` foldr (*) 0 [1..50]
+
+  it "Maybe is an instance of Foldable" $ do
+    foldr' (+) 7 (Just 5) `shouldBe` 12
+    foldr' (+) 2 Nothing  `shouldBe` 2
+    foldr' (+) 2 (Just 4) `shouldBe` 6
