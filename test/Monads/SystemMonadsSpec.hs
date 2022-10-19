6ba54ed175d4dac4bb75fd3b445f47d438f2fb84
Arialdo Martini <arialdomartini@gmail.com>
2022-10-19 08:38:39 +0200

    Chains monadic functions with >>=

--- test/Monads/SystemMonadsSpec.hs
+++ test/Monads/SystemMonadsSpec.hs
@@ -1,3 +1,5 @@
+{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
+{-# HLINT ignore "Redundant bracket" #-}
 module Monads.SystemMonadsSpec where
 
 import Test.Hspec
@@ -22,8 +24,22 @@ instance Monad' Maybe' where
 maybeDouble :: Int -> Maybe' Int
 maybeDouble n = if n < 100 then Just' (n * 2) else Nothing'
 
+
+doubleIfSmall :: Int -> Maybe' Int
+doubleIfSmall n = if n < 100 then Just' (n * 2) else Nothing'
+
+minus1IfEven :: Int -> Maybe' Int
+minus1IfEven n =   if even n then Just' (n -1) else Nothing'
+
+toStringIfOdd :: Int -> Maybe' String
+toStringIfOdd n = if odd n then Just' (show n) else Nothing'
+
 spec :: Spec
 spec = do
   it "Maybe is a Monad" $ do
     Just' 10 >>>= maybeDouble `shouldBe` Just' 20
     Nothing' >>>= maybeDouble `shouldBe` Nothing'
+
+  it "chains monads with bind" $ do
+    (Just' 50) >>>= doubleIfSmall >>>= minus1IfEven >>>= toStringIfOdd `shouldBe` Just' "99"
+    (Just' 200) >>>= doubleIfSmall >>>= minus1IfEven >>>= toStringIfOdd `shouldBe` Nothing'
