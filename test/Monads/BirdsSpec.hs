de42d335f329b1442330ebcfa005a29e9b2c64b6
Arialdo Martini <arialdomartini@gmail.com>
2022-10-23 00:08:43 +0200

    Data clump

--- test/Monads/BirdsSpec.hs
+++ test/Monads/BirdsSpec.hs
@@ -7,8 +7,10 @@ data Pole = Pole { left:: Int, right:: Int } deriving (Eq, Show)
 type Left  = Int
 type Right = Int
 
-walk :: Left -> Right -> Pole -> Maybe Pole
-walk l r pole =
+
+
+walk :: (Left, Right) -> Pole -> Maybe Pole
+walk (l, r) pole =
   let newLeft  = left pole + l
       newRight = right pole + r
       difference = abs (newLeft - newRight) in
@@ -20,18 +22,18 @@ spec :: Spec
 spec = do
   it "falls with 4 birds on the right" $ do
     (return Pole { left = 0, right = 0 }
-      >>= walk 1 0
-      >>= walk 1 0
-      >>= walk 1 0
-      >>= walk 1 0    -- falls here
-      >>= walk (-2) 0) -- too late
+      >>= walk (1, 0)
+      >>= walk (1, 0)
+      >>= walk (1, 0)
+      >>= walk (1, 0)    -- falls here
+      >>= walk ((-2), 0)) -- too late
       `shouldBe` Nothing
 
   it "walks if never exceeds the threshold" $ do
     (return Pole { left = 0, right = 0 }
-      >>= walk 1 0
-      >>= walk 1 0
-      >>= walk (-1) 0
-      >>= walk 0 2
-      >>= walk 1 1)
+      >>= walk (1, 0)
+      >>= walk (1, 0)
+      >>= walk ((-1), 0)
+      >>= walk (0, 2)
+      >>= walk (1, 1))
       `shouldBe` Just Pole { left= 2, right= 3 }
