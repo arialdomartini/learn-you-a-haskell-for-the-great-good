b921c25153eb21699f5502c3de629850ab663530
Arialdo Martini <arialdomartini@gmail.com>
2022-10-25 21:47:43 +0200

    Monads laws: Right Identity

--- test/Monads/LawsSpec.hs
+++ test/Monads/LawsSpec.hs
@@ -1,5 +1,7 @@
 {-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
 {-# HLINT ignore "Monad law, left identity" #-}
+{-# OPTIONS_GHC -Wno-type-defaults #-}
+{-# HLINT ignore "Monad law, right identity" #-}
 module Monads.LawsSpec where
 
 import Test.Hspec
@@ -9,9 +11,21 @@ import Test.Hspec
 f :: Int -> Maybe Int
 f n = Just $ n * 42
 
+{-
+
+Monads Laws
+
+return x >>= f  = f x
+m x >>= return  = m x
+
+-}
+
 spec :: Spec
 spec = do
+  -- applying return on the left of  a bind, is like just applying the function
   it "left identity" $ do
-    (return 3 >>= f)
-      `shouldBe`
-     f 3
+    (return 3 >>= f)     `shouldBe`    f 3
+
+  -- applying return on the right of a bind does noth change the monadic input
+  it "right identity" $ do
+    (Just 3 >>= return)  `shouldBe`    Just 3
