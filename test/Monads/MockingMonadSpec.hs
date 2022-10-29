c45742d1363383ede53c5647b0a5c415fdb2948c
Arialdo Martini <arialdomartini@gmail.com>
2022-10-29 23:39:46 +0200

    Mockig non IO functions

--- test/Monads/MockingMonadSpec.hs
+++ test/Monads/MockingMonadSpec.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE DeriveFunctor #-}
 module Monads.MockingMonadSpec where
 
 import Test.Hspec
@@ -25,6 +26,27 @@ instance MonadFS IO where
   myWriteFile = writeFile
 
 
+class Functor a => Result a where
+  length' :: String -> a Int
+
+data Mock a = Mock a deriving (Functor, Show, Eq)
+data Prod a = Prod a deriving (Functor, Show, Eq)
+
+instance Result Mock where
+  length' s = Mock 100
+
+instance Result Prod where
+  length' s = Prod (length s)
+
+elevated :: Int -> Int
+elevated = (^(8::Int))
+
+-- calculate :: Result a => Int -> a String
+calculate :: (Result f) => Int -> f String
+calculate n =
+  let r = length' (show  (elevated n)) in
+    fmap show r
+
 newtype MockFS a = MockFS (Identity a) deriving (Functor, Applicative, Monad, Show, Eq)
 
 instance MonadFS MockFS where
@@ -35,3 +57,9 @@ spec :: Spec
 spec = do
   it "should mock" $ do
     (withSideEffects "foo.txt" "new content" :: MockFS String) `shouldBe` MockFS (return "content of file foo.txt+new content")
+
+  it "should mock non IO operations" $ do
+    (calculate 2 :: Prod String)`shouldBe` (Prod "3")
+
+  it "should mock non IO operations" $ do
+    (calculate 2 :: Mock String)`shouldBe` (Mock "100")
