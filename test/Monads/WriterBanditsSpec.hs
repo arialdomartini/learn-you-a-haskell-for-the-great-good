3a19db1604c3d23d431a67767a79c1e381ed4a5c
Arialdo Martini <arialdomartini@gmail.com>
2022-10-30 22:40:54 +0100

    Writing the Writer Monad: a function returning a tuple

---
+++ test/Monads/WriterBanditsSpec.hs
@@ -0,0 +1,13 @@
+module Monads.WriterBanditsSpec where
+
+import Test.Hspec
+
+isBigGang :: Int -> (Bool, String)
+isBigGang n = (n > 9, "compared gang size of " <> show n <> " to 9")
+
+spec :: Spec
+spec = do
+  it "compares gang size" $ do
+    isBigGang 9 `shouldBe`  (False, "compared gang size of 9 to 9")
+    isBigGang 30 `shouldBe` (True, "compared gang size of 30 to 9")
+    isBigGang 3 `shouldBe`  (False, "compared gang size of 3 to 9")
