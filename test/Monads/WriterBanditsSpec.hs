79eeeb7a09087dedf7b6fa221fa7a5c079cd7393
Arialdo Martini <arialdomartini@gmail.com>
2022-10-30 22:52:24 +0100

    Concatening lists instead of strings

--- test/Monads/WriterBanditsSpec.hs
+++ test/Monads/WriterBanditsSpec.hs
@@ -5,6 +5,10 @@ import Test.Hspec
 isBigGang :: Int -> (Bool, String)
 isBigGang n = (n > 9, "compared gang size of " <> show n <> " to 9")
 
+isBigGangList :: Int -> (Bool, [String])
+isBigGangList n = (n > 9, ["compared gang size of " <> show n <> " to 9"])
+
+
 applyLog :: Semigroup l => (a, l) -> (a -> (b, l)) -> (b, l)
 applyLog (v, l') f =
   let (v'', l'') = f v in
@@ -19,5 +23,5 @@ spec = do
     isBigGang 3 `shouldBe`  (False, "compared gang size of 3 to 9")
 
   it "feeds a tuple to a function returning a tuple" $ do
-    let smallGang = (3, "smallish gang,") in
-      smallGang `applyLog` isBigGang `shouldBe` (False, "smallish gang,compared gang size of 3 to 9")
+    let smallGang = (3, ["smallish gang"]) in
+      smallGang `applyLog` isBigGangList `shouldBe` (False, ["smallish gang", "compared gang size of 3 to 9"])
