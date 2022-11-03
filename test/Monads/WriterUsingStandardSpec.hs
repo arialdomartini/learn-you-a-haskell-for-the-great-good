0952000898c1e229ca23888ecbd13acfb052b671
Arialdo Martini <arialdomartini@gmail.com>
2022-11-03 09:26:28 +0100

    Using do notation with standard Writer monad

--- test/Monads/WriterUsingStandardSpec.hs
+++ test/Monads/WriterUsingStandardSpec.hs
@@ -9,9 +9,20 @@ f =
   \s -> writer (s ++ "!", ["screaming"]) >>=
   \s -> writer (length s, ["calculating length"])
 
+f' :: Writer [String] Int
+f' = do
+  let s = "Hey"
+  s' <- writer (s ++ "!", ["screaming"])
+  writer (length s', ["calculating length"])
+
 spec :: Spec
 spec = do
   it "bind monadic functions" $ do
     let (result, logs) = runWriter f
+      in do logs `shouldBe` ["screaming", "calculating length"]
+            result `shouldBe` 4
+
+  it "bind monadic functions, with do notation" $ do
+    let (result, logs) = runWriter f'
       in do logs `shouldBe` ["screaming", "calculating length"]
             result `shouldBe` 4
