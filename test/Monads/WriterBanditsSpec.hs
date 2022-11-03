e991946682b84fae6ec9b1e6491c94c8b2141165
Arialdo Martini <arialdomartini@gmail.com>
2022-11-03 07:44:25 +0100

    Reusing applyLog for adding up prices, with Monoid.Sum

--- test/Monads/WriterBanditsSpec.hs
+++ test/Monads/WriterBanditsSpec.hs
@@ -1,18 +1,34 @@
 module Monads.WriterBanditsSpec where
 
 import Test.Hspec
+import Data.Monoid (Sum(Sum))
 
 isBigGang :: Int -> (Bool, String)
 isBigGang n = (n > 9, "compared gang size of " <> show n <> " to 9")
 
 isBigGangList :: Int -> (Bool, [String])
-isBigGangList n = (n > 9, ["compared gang size of " <> show n <> " to 9"])
+isBigGangList n = (n > 9, pure $ "compared gang size of " <> show n <> " to 9")
 
 
 applyLog :: Semigroup l => (a, l) -> (a -> (b, l)) -> (b, l)
 applyLog (v, l') f =
   let (v'', l'') = f v in
     (v'', l' <> l'')
+
+
+type Food = String
+type Drink = String
+type Price = Sum Int
+
+addDrink :: Food -> (Drink, Price)
+addDrink "beans" = ("whiskey", Sum 10)
+addDrink "pasta" = ("wine", Sum 5)
+addDrink "bisquits" = ("milk", Sum 2)
+addDrink _ = ("beer", Sum 2)
+
+
+
+
 
 
 spec :: Spec
@@ -23,5 +39,8 @@ spec = do
     isBigGang 3 `shouldBe`  (False, "compared gang size of 3 to 9")
 
   it "feeds a tuple to a function returning a tuple" $ do
-    let smallGang = (3, ["smallish gang"]) in
+    let smallGang = (3, pure "smallish gang") in
       smallGang `applyLog` isBigGangList `shouldBe` (False, ["smallish gang", "compared gang size of 3 to 9"])
+
+  it "uses applyLog to calculate total cost" $ do
+    ("beans", Sum 100) `applyLog` addDrink `shouldBe` ("whiskey", Sum (100 + 10))
