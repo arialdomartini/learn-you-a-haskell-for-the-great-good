5aa5e69e7a8f079b33d053fa98c9d00a9adc9bb9
Arialdo Martini <arialdomartini@gmail.com>
2022-11-03 09:30:18 +0100

    Testing Writer return

--- test/Monads/WriterUsingStandardSpec.hs
+++ test/Monads/WriterUsingStandardSpec.hs
@@ -7,7 +7,7 @@ f :: Writer [String] Int
 f =
   return "Hey" >>=
   \s -> writer (s ++ "!", ["screaming"]) >>=
-  \s -> writer (length s, ["calculating length"])
+  \s' -> writer (length s', ["calculating length"])
 
 f' :: Writer [String] Int
 f' = do
@@ -26,3 +26,6 @@ spec = do
     let (result, logs) = runWriter f'
       in do logs `shouldBe` ["screaming", "calculating length"]
             result `shouldBe` 4
+
+  it "uses default values" $ do
+    runWriter (return 3 :: Writer String Int) `shouldBe` (3, "")
