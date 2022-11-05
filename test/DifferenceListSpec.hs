c00fe99bb2c3a97b54668fc004ff91148aabff97
Arialdo Martini <arialdomartini@gmail.com>
2022-11-05 21:15:15 +0100

    DiffList as a newtype

--- test/DifferenceListSpec.hs
+++ test/DifferenceListSpec.hs
@@ -2,16 +2,18 @@ module DifferenceListSpec where
 
 import Test.Hspec
 
-type DiffList a = [a] -> [a]
+newtype DiffList a = DiffList { getDiffList :: [a] -> [a] }
 
 diffList :: [a] -> DiffList a
 -- diffList eq xs = eq ++ xs
-diffList eq = \xs -> eq ++ xs
-
-
+-- diffList eq = \xs -> eq ++ xs
+diffList eq = DiffList (eq ++)
 
 append :: DiffList a -> DiffList a -> DiffList a
-f `append` g = \xs -> f (g xs)
+-- f `append` g = \xs -> f (g xs)
+append f g = DiffList (f' . g') where
+  f' = getDiffList f
+  g' = getDiffList g
 
 spec :: Spec
 spec = do
@@ -19,4 +21,4 @@ spec = do
     let l1  = diffList ([1,2,3]:: [Int])
         l2  = diffList ([10,20,30]:: [Int])
         l12 = l1 `append` l2 in
-      l12 [] `shouldBe` diffList ([1,2,3,10,20,30]:: [Int]) []
+      getDiffList l12 [] `shouldBe` getDiffList (diffList ([1,2,3,10,20,30]:: [Int])) []
