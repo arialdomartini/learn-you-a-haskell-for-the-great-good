ffb5e374b3b721e66ae7d3e2ba71817c2e4e850f
Arialdo Martini <arialdomartini@gmail.com>
2022-11-06 08:39:54 +0100

    binding 2 functions using the do notation

--- test/Monads/ReaderSpec.hs
+++ test/Monads/ReaderSpec.hs
@@ -8,7 +8,16 @@ f s = \x -> length (s ++ x)
 g :: Int -> (String -> Int)
 g n = \x -> (length x) + (n *2)
 
+bound :: String -> Int
+bound = do
+  r  <- f "hey"
+  r' <- g r
+  return r'
+
 spec :: Spec
 spec = do
   it "binds 2 functions" $ do
     ((f "hey") >>= g)("joe") `shouldBe` 15
+
+  it "binds 2 functions using the do notation" $ do
+    bound "joe" `shouldBe` 15
