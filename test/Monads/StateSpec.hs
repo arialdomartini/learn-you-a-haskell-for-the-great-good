4e090f147d55347d342f8447d6ad031a5e7684ec
Arialdo Martini <arialdomartini@gmail.com>
2022-11-06 14:00:20 +0100

    State as a functor

--- test/Monads/StateSpec.hs
+++ test/Monads/StateSpec.hs
@@ -1,15 +1,28 @@
 {-# LANGUAGE GADTs #-}
+{-# LANGUAGE InstanceSigs #-}
 module Monads.StateSpec where
 
 import Test.Hspec
 
-data State' a s where
-  State' :: {runState :: s -> (a, s)} -> State' a s
+data State' s a where
+  State' :: {runState :: s -> (a, s)} -> State' s a
 
-len :: Float -> State' String Int
-len n = State' (\count -> (show n, count+1))
+instance Functor (State' s) where
+  fmap :: (a -> b) -> State' s a -> State' s b
+  fmap f st =
+    State' (\s ->
+              let fs = runState st
+                  (v,s') = fs s in
+                (f v, s'))
+
+toString :: Float -> State' Int String
+toString n = State' (\count -> (show n, count+1))
 
 spec :: Spec
 spec = do
   it "runState" $ do
-    runState (len 5.3) 10 `shouldBe` ("5.3", 11)
+    runState (toString 5.3) 10 `shouldBe` ("5.3", 11)
+
+  it "has an instance of Functor" $ do
+    let r = fmap (++"!") (toString 5.3) in
+        runState r 99 `shouldBe` ("5.3!", 100)
