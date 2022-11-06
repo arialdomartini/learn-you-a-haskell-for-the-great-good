9bbcf83dd968f708e6f57cf081f1d7d549847937
Arialdo Martini <arialdomartini@gmail.com>
2022-11-06 14:23:16 +0100

    State as an applicative functor

--- test/Monads/StateSpec.hs
+++ test/Monads/StateSpec.hs
@@ -1,5 +1,6 @@
 {-# LANGUAGE GADTs #-}
 {-# LANGUAGE InstanceSigs #-}
+{-# LANGUAGE TupleSections #-}
 module Monads.StateSpec where
 
 import Test.Hspec
@@ -11,12 +12,27 @@ instance Functor (State' s) where
   fmap :: (a -> b) -> State' s a -> State' s b
   fmap f st =
     State' (\s ->
-              let fs = runState st
-                  (v,s') = fs s in
+              let (v,s') = runState st s in
                 (f v, s'))
+
+instance Applicative (State' s) where
+  pure :: a -> State' s a
+  pure a = State' (a, )
+  (<*>) :: State' s (a -> b) -> State' s a -> State' s b
+  sf <*> sv =
+    State' (\s ->
+              let (f, s') = runState sf s
+                  (v, s'') = runState sv s' in
+                (f v, s''))
 
 toString :: Float -> State' Int String
 toString n = State' (\count -> (show n, count+1))
+
+fs :: State' Int (String -> String)
+fs = State' (\count -> ((++ "!"), count + 1))
+
+vs :: State' Int String
+vs = State' (\count -> ("Hey", count+1))
 
 spec :: Spec
 spec = do
@@ -26,3 +42,6 @@ spec = do
   it "has an instance of Functor" $ do
     let r = fmap (++"!") (toString 5.3) in
         runState r 99 `shouldBe` ("5.3!", 100)
+
+  it "has an instance of Applicative" $ do
+    runState (fs <*> vs) 100 `shouldBe` ("Hey!", 102)
