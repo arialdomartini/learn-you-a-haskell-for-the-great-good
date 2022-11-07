3b0f986a0828a4867003f366a4eff2426d4f7442
Arialdo Martini <arialdomartini@gmail.com>
2022-11-07 22:36:45 +0100

    limtM with Writer

--- test/Monads/MonadicFunctionsSpec.hs
+++ test/Monads/MonadicFunctionsSpec.hs
@@ -1,13 +1,19 @@
 {-# OPTIONS_GHC -Wno-type-defaults #-}
 {-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
 {-# HLINT ignore "Use fmap" #-}
+
 module Monads.MonadicFunctionsSpec where
 
 import Test.Hspec
-import Control.Monad (liftM)
+import Control.Monad.Writer
+
+
 
 
 spec :: Spec
 spec = do
-  it "liftM" $ do
-    liftM (*2) (return 3) `shouldBe` fmap (*2) (Just 3)
+  it "runs liftM" $ do
+    (liftM (*2) (return 3)) `shouldBe` (fmap (*2) (Just 3))
+
+  it "runs liftM with Writer" $ do
+    runWriter (liftM not $ writer (True, "negated")) `shouldBe` (False, "negated")
