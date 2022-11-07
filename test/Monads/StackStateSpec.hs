a0f1da560c100c2f63b121b7cd5e1f5e682137a6
Arialdo Martini <arialdomartini@gmail.com>
2022-11-07 07:57:59 +0100

    pop and push with MonadState

--- test/Monads/StackStateSpec.hs
+++ test/Monads/StackStateSpec.hs
@@ -1,10 +1,13 @@
 {-# OPTIONS_GHC -Wno-type-defaults #-}
 {-# OPTIONS_GHC -Wno-incomplete-uni-patterns #-}
 {-# OPTIONS_GHC -Wno-unused-do-bind #-}
+{-# OPTIONS_GHC -Wno-orphans #-}
+
 module Monads.StackStateSpec where
 
 import Test.Hspec ( shouldBe, it, Spec )
 import Control.Monad.State
+import qualified Control.Monad.Identity as Data.Functor
 
 type Stack a = [a]
 
@@ -22,6 +25,11 @@ useStack = do
     a <- pop'
     return a
 
+-- This is required by (x:xs) <- get in pop'': I don't know why!
+instance MonadFail Data.Functor.Identity where
+  fail = error "should not happen"
+
+
 anotherUseOfStack :: State (Stack Int) Int
 anotherUseOfStack = do
   push' 100
@@ -38,6 +46,25 @@ combinedDo = do
   _ <- useStack
   anotherUseOfStack
 
+pop'' :: State (Stack Int) Int
+pop'' = do
+  (x:xs) <- get
+  put xs
+  return x
+
+push'' :: Int -> State (Stack Int) ()
+push'' x = do
+  xs <- get
+  put (x:xs)
+  return ()
+
+useMonadState :: State (Stack Int) Int
+useMonadState = do
+  push'' 1
+  push'' 2
+  push'' 3
+  pop''
+
 spec :: Spec
 spec = do
   it "use a stack with Reader" $ do
@@ -48,3 +75,6 @@ spec = do
 
   it "combines 2 stateful computations, using do notation" $ do
     runState combinedDo [] `shouldBe` (2, [1])
+
+  it "implements pop and put with MonadState" $ do
+    runState useMonadState [] `shouldBe` (3, [2,1])
