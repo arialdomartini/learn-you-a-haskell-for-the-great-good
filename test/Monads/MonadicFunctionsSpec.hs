f8917d284f3864d9265fe3233d60d3dc1aecf2c7
Arialdo Martini <arialdomartini@gmail.com>
2022-11-08 23:59:59 +0100

    liftM2 is liftA2 for Monads

--- test/Monads/MonadicFunctionsSpec.hs
+++ test/Monads/MonadicFunctionsSpec.hs
@@ -6,6 +6,16 @@ module Monads.MonadicFunctionsSpec where
 
 import Test.Hspec
 import Control.Monad.Writer
+import Control.Applicative (liftA2)
+
+f :: Int -> Int -> Int
+f a b = a + b
+
+fA2 :: Maybe Int -> Maybe Int -> Maybe Int
+fA2 = liftA2 f
+
+fM2 :: Maybe Int -> Maybe Int -> Maybe Int
+fM2 = liftM2 f
 
 
 spec :: Spec
@@ -17,5 +27,9 @@ spec = do
     runWriter (liftM not $ writer (True, "negated")) `shouldBe` (False, "negated")
 
   it "ap is <*> for Monads" $ do
-    let f a b c = a + b + c in
-      Just f `ap` Just 1 `ap` Just 2 `ap` Just 3 `shouldBe` Just f <*> Just 1 <*> Just 2 <*> Just 3
+    Just f `ap` Just 1 `ap` Just 2 `shouldBe` Just f <*> Just 1 <*> Just 2
+
+  it "liftA2 for Monads" $ do
+    f 1 2 `shouldBe` 3
+    fA2 (Just 1) (Just 2) `shouldBe` Just 3
+    fM2 (Just 1) (Just 2) `shouldBe` Just 3
