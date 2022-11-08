cd4fa0dce6daeca529dbe3425c25b2b5da0e75b0
Arialdo Martini <arialdomartini@gmail.com>
2022-11-08 07:45:15 +0100

    ap is <*> for Monads

--- test/Monads/MonadicFunctionsSpec.hs
+++ test/Monads/MonadicFunctionsSpec.hs
@@ -8,12 +8,14 @@ import Test.Hspec
 import Control.Monad.Writer
 
 
-
-
 spec :: Spec
 spec = do
-  it "runs liftM" $ do
-    (liftM (*2) (return 3)) `shouldBe` (fmap (*2) (Just 3))
+  it "liftM is fmap for Monads" $ do
+    liftM (*2) (return 3) `shouldBe` fmap (*2) (Just 3)
 
   it "runs liftM with Writer" $ do
     runWriter (liftM not $ writer (True, "negated")) `shouldBe` (False, "negated")
+
+  it "ap is <*> for Monads" $ do
+    let f a b c = a + b + c in
+      Just f `ap` Just 1 `ap` Just 2 `ap` Just 3 `shouldBe` Just f <*> Just 1 <*> Just 2 <*> Just 3
