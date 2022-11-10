70bd4bc1fbe6b413c2dab523f747feb961ba130b
Arialdo Martini <arialdomartini@gmail.com>
2022-11-10 22:11:23 +0100

    Modifies a very nested structure recursively consuming directions

--- test/ZipperSpec.hs
+++ test/ZipperSpec.hs
@@ -21,6 +21,38 @@ freeTree =
                           (Node 'A' Empty Empty)
                           (Node 'C' Empty Empty)))
 
+expected :: Tree Char
+expected =
+  Node 'P'
+        (Node 'O'
+                (Node 'L' (Node 'N' Empty Empty)
+                (Node 'T' Empty Empty))
+        (Node 'Y'
+                (Node 'S' Empty Empty)
+                (Node 'A' Empty Empty)))
+        (Node 'L'
+                (Node 'X' (Node 'C' Empty Empty)   -- <-- X here
+                          (Node 'R' Empty Empty))
+                 (Node 'A'
+                          (Node 'A' Empty Empty)
+                          (Node 'C' Empty Empty)))
+
+
+data Direction = L | R deriving (Eq, Show)
+type Directions = [Direction]
+
+
+changeElement :: a -> Directions -> Tree a -> Tree a
+changeElement v' [] (Node v l r) = Node v' l r
+changeElement v' (L:ds) (Node v l r) =
+  Node v changed r
+  where changed = changeElement v' ds l
+changeElement v' (R:ds) (Node v l r) =
+  Node v l changed
+  where changed = changeElement v' ds r
+
+
+
 spec :: Spec
 spec = do
   it "should modify W in X" $ do
@@ -32,17 +64,7 @@ spec = do
         nr = Node value nl' r'
         nroot = Node root l nr in
 
-      nroot `shouldBe`
-       Node 'P'
-        (Node 'O'
-                (Node 'L' (Node 'N' Empty Empty)
-                (Node 'T' Empty Empty))
-        (Node 'Y'
-                (Node 'S' Empty Empty)
-                (Node 'A' Empty Empty)))
-        (Node 'L'
-                (Node 'X' (Node 'C' Empty Empty)   -- <-- This W
-                          (Node 'R' Empty Empty))
-                 (Node 'A'
-                          (Node 'A' Empty Empty)
-                          (Node 'C' Empty Empty)))
+      nroot `shouldBe` expected
+
+  it "should modify W in X using directions" $ do
+    changeElement 'X' [R,L] freeTree `shouldBe` expected
