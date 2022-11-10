c87857da793b1834226453ee254e63617487c1e4
Arialdo Martini <arialdomartini@gmail.com>
2022-11-10 14:47:01 +0100

    Probs (as a collection of probabilities) is a Functor

--- test/Monads/ProbabilitySpec.hs
+++ test/Monads/ProbabilitySpec.hs
@@ -1,17 +1,18 @@
 {-# OPTIONS_GHC -Wno-type-defaults #-}
+{-# LANGUAGE InstanceSigs #-}
 module Monads.ProbabilitySpec where
 
 import Test.Hspec
 import Data.Ratio
 
-newtype Probability a = Probability {getProbability :: (a, Rational)} deriving (Eq, Show)
+type Prob a = (a, Rational)
+newtype Probs a = Probs {getProbabilities :: [Prob a]} deriving (Eq, Show)
 
-instance Functor Probability where
-  fmap f prob =
-    let (v, p) = getProbability prob in
-      Probability (f v, p )
+instance Functor Probs where
+  fmap f (Probs probs) = Probs (fmap (\(v,p) -> (f v, p)) probs)
+
 
 spec :: Spec
 spec = do
   it "is a functor" $ do
-    fmap (*2) (Probability (100, 1%5)) `shouldBe` Probability (200, 1%5)
+    fmap (*2) (Probs [(100, 1%5), (90, 3%5), (1, 2%5)]) `shouldBe` Probs  [(200, 1%5), (180, 3%5), (2, 2%5)]
