9c52b0d4bdbea16dd3bf3cfb406b01d1332f52e1
Arialdo Martini <arialdomartini@gmail.com>
2022-11-10 07:39:28 +0100

    A safe RPN calculator, using foldM

--- test/Monads/SafeRPNSpec.hs
+++ test/Monads/SafeRPNSpec.hs
@@ -1,6 +1,7 @@
 module Monads.SafeRPNSpec where
 
 import Test.Hspec
+import Control.Monad (foldM)
 
 type Value = Double
 type Stack = [Value]
@@ -13,6 +14,25 @@ folding (a:b:xs) "+"  = (b + a) : xs
 folding (a:b:xs) "*" = (b * a) : xs
 folding xs n = (read n:: Value) : xs
 
+-- foldM (b -> a -> m b) b (t a)
+
+--foldM' :: (b -> a -> m b) -> b -> t a -> m b
+
+calcSafe :: String -> Maybe Value
+calcSafe = head . sequence . foldM folding' [] . words
+
+readMaybe :: Read a => String -> Maybe a
+readMaybe s = case reads s of
+  [(x, "")] -> Just x
+  _ -> Nothing
+
+folding' :: Stack -> String -> Maybe Stack
+folding' (a:b:xs) "+"  = Just ((b + a) : xs)
+folding' (a:b:xs) "*" = Just ((b * a) : xs)
+folding' xs n = fmap (:xs) (readMaybe n)
+
+
+
 
 spec :: Spec
 spec = do
@@ -20,3 +40,7 @@ spec = do
     calc "4" `shouldBe` 4
     calc "4 2 +" `shouldBe` 6
     calc "4 2 + 3 3 + *" `shouldBe` (4 + 2) * (3 + 3)
+
+  it "safe RPN calculator" $ do
+    calcSafe "4 2 + 3 3 + *" `shouldBe` Just ((4 + 2) * (3 + 3))
+    calcSafe "+ 3 3 + *" `shouldBe` Nothing
