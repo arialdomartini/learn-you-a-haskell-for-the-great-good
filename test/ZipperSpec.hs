3e4b2fc797bb590a16372fac1637ace5b2bf70f8
Arialdo Martini <arialdomartini@gmail.com>
2022-11-11 23:34:54 +0100

    Zipper, go up and down

--- test/ZipperSpec.hs
+++ test/ZipperSpec.hs
@@ -27,8 +27,30 @@ modifyRoot :: (a -> a) -> Zipper a -> Zipper a
 modifyRoot _ (Empty, bs) = (Empty, bs)
 modifyRoot f (Node v l r, bs) = (Node (f v) l r, bs)
 
+
+goLeft :: Zipper a -> Zipper a
+goLeft (Empty, _) = error "You reached the end"
+goLeft (Node v l r, bs) = (l, LeftCrumb v r:bs)
+
+
+goRight :: Zipper a -> Zipper a
+goRight (Empty, _) = error "You reached the end"
+goRight (Node v l r, bs) = (r, RightCrumb v l:bs)
+
+goUp :: Zipper a -> Zipper a
+goUp (r, (RightCrumb v l):bs) = (Node v l r, bs)
+goUp (l, (LeftCrumb v r):bs) = (Node v l r, bs)
+goUp (_, []) = error "You cannot go over the top"
+
+(-:) :: a -> (a -> b) -> b
+a -: f = f a
+
 spec :: Spec
 spec = do
   it "should modify root" $ do
     modifyRoot (++"!") (simpleTree, []) `shouldBe`
         (Node "top!" (Node "left" Empty Empty) (Node "right" Empty Empty), [])
+
+  it "should modify an arbitary node" $ do
+    (simpleTree, []) -: goRight -: modifyRoot (++"!") -: goUp -: modifyRoot (++"!!!") `shouldBe`
+        (Node "top!!!" (Node "left" Empty Empty) (Node "right!" Empty Empty),[])
